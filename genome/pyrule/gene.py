fa4162128bd2a606c31d85b2a3494dd17b431fdb
Hwrn <hwrn.aou@sjtu.edu.cn>
2023-07-22 20:36:59 +0800

    fmt: remove dup codes

--- genome/pyrule/gene.py
+++ genome/pyrule/gene.py
@@ -2,7 +2,7 @@
 """
  * @Date: 2023-07-22 15:34:34
  * @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
- * @LastEditTime: 2023-07-22 20:29:47
+ * @LastEditTime: 2023-07-22 20:34:42
  * @FilePath: /genome/genome/pyrule/gene.py
  * @Description:
 """
@@ -20,19 +20,20 @@ mantis_config = cache["config"]["mantis_config"]
 mantis_config_check = mantis_config + ".check"
 
 
+mantis_setup_shellcmd = """
+mantis setup \
+    --mantis_config {input.mantis_config} \
+    -c {threads}
+"""
+
+
 @workflow.rule(name="annotate_gene_mantis_check")
 @workflow.input(mantis_config=mantis_config)
 @workflow.output(mantis_config_check=touch(mantis_config_check))
 @workflow.threads(64)
 @workflow.conda(envs_dir / "mantis.yaml")
 @workflow.shadow("shallow")
-@workflow.shellcmd(
-    """
-        mantis setup \
-            --mantis_config {input.mantis_config} \
-            -c {threads}
-        """
-)
+@workflow.shellcmd(mantis_setup_shellcmd)
 @workflow.run
 def __rule_annotate_gene_mantis_check(
     input,
@@ -62,14 +63,27 @@ def __rule_annotate_gene_mantis_check(
     __is_snakemake_rule_func=True,
 ):
     shell(
-        """
-        mantis setup \
-            --mantis_config {input.mantis_config} \
-            -c {threads}
-        """,
+        mantis_setup_shellcmd,
         bench_record=bench_record,
         bench_iteration=bench_iteration,
     )
+
+
+mantis_run_shellcmd = """
+rm -f smk-mantis
+
+mantis \
+    run \
+    --mantis_config {params.mantis_config} \
+    -c {threads} \
+    -i {input.faa} \
+    -o smk-mantis
+
+mv smk-mantis/consensus_annotation.tsv \
+    {output.annot}
+mv smk-mantis \
+    {input.faa}-mantis/
+"""
 
 
 @workflow.rule(name="annotate_gene_mantis")
@@ -80,23 +94,7 @@ def __rule_annotate_gene_mantis_check(
 @workflow.conda(envs_dir / "mantis.yaml")
 @workflow.wildcard_constraints(method="mantis")
 @workflow.shadow("shallow")
-@workflow.shellcmd(
-    """
-        rm -f smk-mantis
-
-        mantis \
-            run \
-            --mantis_config {params.mantis_config} \
-            -c {threads} \
-            -i {input.faa} \
-            -o smk-mantis
-
-        mv smk-mantis/consensus_annotation.tsv \
-           {output.annot}
-        mv smk-mantis \
-            {input.faa}-mantis/
-        """
-)
+@workflow.shellcmd(mantis_run_shellcmd)
 @workflow.run
 def __rule_annotate_gene_mantis(
     input,
@@ -126,21 +124,7 @@ def __rule_annotate_gene_mantis(
     __is_snakemake_rule_func=True,
 ):
     shell(
-        """
-        rm -f smk-mantis
-
-        mantis \
-            run \
-            --mantis_config {params.mantis_config} \
-            -c {threads} \
-            -i {input.faa} \
-            -o smk-mantis
-
-        mv smk-mantis/consensus_annotation.tsv \
-           {output.annot}
-        mv smk-mantis \
-            {input.faa}-mantis/
-        """,
+        mantis_run_shellcmd,
         bench_record=bench_record,
         bench_iteration=bench_iteration,
     )
