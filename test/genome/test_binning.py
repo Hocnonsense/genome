96bef912ba3833d01594654635cdc1369f6c912a
Hwrn <hwrn.aou@sjtu.edu.cn>
2023-10-23 13:24:38 +0800

    feat: allow relative path inference in yaml

--- test/genome/test_binning.py
+++ test/genome/test_binning.py
@@ -2,7 +2,7 @@
 """
  * @Date: 2022-10-25 20:53:06
  * @LastEditors: Hwrn hwrn.aou@sjtu.edu.cn
- * @LastEditTime: 2023-10-22 21:33:12
+ * @LastEditTime: 2023-10-23 13:22:27
  * @FilePath: /genome/test/genome/test_binning.py
  * @Description:
 __file__ = "test/genome/test_binning.py"
@@ -25,14 +25,7 @@ from genome.binning import (
 try:
     from ._decorator import temp_output, test_temp, test_files
 except (ModuleNotFoundError, ImportError):
-    try:
-        from test.genome._decorator import (  # type: ignore
-            temp_output,
-            test_temp,
-            test_files,
-        )
-    except (ModuleNotFoundError, ImportError):
-        from _decorator import temp_output, test_temp, test_files
+    from _decorator import temp_output, test_temp, test_files
 
 
 @temp_output
@@ -41,7 +34,7 @@ def test_binning_input_to_config(test_temp: Path):
         contig=str(test_files / "binny_contigs_4bins.fa"),
         lsbams=check_bams(test_temp / "binny_test_binning", []),
         jgi=str(test_files / "binny_reads_4bins-jgi.tsv"),
-    ).dump_prefix(test_temp / "binny_test_binning")
+    ).dump_prefix(test_temp / "binny_test_binning", relpath=False)
 
     assert f == Path(test_temp / "binny_test_binning-bins.yaml")
     with open(f) as yi:
@@ -54,14 +47,9 @@ def test_binning_input_to_config(test_temp: Path):
 
 @temp_output
 def test_binning_config_to_config(test_temp: Path):
-    f = BinningInput(
-        contig=str(test_files / "binny_contigs_4bins.fa"),
-        lsbams=check_bams(test_temp / "binny_test_binning", [])[0],
-        jgi=str(test_files / "binny_reads_4bins-jgi.tsv"),
-    ).dump_prefix(test_temp / "binny_test_binning")
-    f1 = BinningConfig(MIN_BIN_CONTIG_LEN=2500, bin_config=f).to_config(
-        test_temp / "test_binning_config.yaml"
-    )
+    f1 = BinningConfig(
+        MIN_BIN_CONTIG_LEN=2500, bin_config=test_temp / "binny_test_binning-bins.yaml"
+    ).to_config(test_temp / "test_binning_config.yaml")
     with open(f1) as yi:
         assert yaml.safe_load(yi) == {
             "MIN_BIN_CONTIG_LEN": 2500,
